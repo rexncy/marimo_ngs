79ddfd636fbbfb501ca8d29ffa053f7c1619c014
rexncy <rexncy@gmail.com>
2025-07-04 21:58:23 +0800

    Fix port

--- main.py
+++ main.py
@@ -130,6 +130,6 @@ if __name__ == "__main__":
     uvicorn.run(
         app,
         host=os.getenv("HOST", "0.0.0.0"),
-        port=int(os.getenv("PORT", 8080)),
+        port=8080,
         log_level="info",
     )

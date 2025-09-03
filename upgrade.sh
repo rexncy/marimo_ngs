8d27b5bd0a4749c047b78041ae62e472202b3566
rexncy <rexncy@gmail.com>
2025-09-03 16:18:28 +0800

    fix upgrade.sh with -d

--- upgrade.sh
+++ upgrade.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker compose -f docker-compose.prod.yml stop && gh repo sync && docker compose -f docker-compose.prod.yml build && docker compose -f docker-compose.prod.yml up
+docker compose -f docker-compose.prod.yml stop && gh repo sync && docker compose -f docker-compose.prod.yml build && docker compose -f docker-compose.prod.yml up -d
